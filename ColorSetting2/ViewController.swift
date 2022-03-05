122bef176dc36066a03f3f8d4aff91b8be4121e2
Aleksey Zarudny <zarudnyy2@gmail.com>
2022-03-05 22:26:47 +0300

    setup logic

--- ColorSetting2/ViewController.swift
+++ ColorSetting2/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var rSlider: UISlider!
+    @IBOutlet var gSlider: UISlider!
+    @IBOutlet var bSlider: UISlider!
+    
+    @IBOutlet var rLabel: UILabel!
+    @IBOutlet var gLabel: UILabel!
+    @IBOutlet var bLabel: UILabel!
+    
+    @IBOutlet var rView: UILabel!
+    @IBOutlet var gView: UILabel!
+    @IBOutlet var bView: UILabel!
+    
+    private func changeColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(rSlider.value), green: CGFloat(gSlider.value), blue: CGFloat(bSlider.value), alpha: 1)
+    }
+    private func changeView() {
+        rView.text = String(round(rSlider.value * 100) / 100)
+        gView.text = String(round(gSlider.value * 100) / 100)
+        bView.text = String(round(bSlider.value * 100) / 100)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 20
+        
+        rLabel.text = "Red"
+        gLabel.text = "Green"
+        bLabel.text = "Blue"
+        
+        rSlider.minimumTrackTintColor = .red
+        gSlider.minimumTrackTintColor = .green
+        bSlider.minimumTrackTintColor = .blue
+        
+        }
+        
+    @IBAction func setupColor() {
+        changeColor()
+        changeView()
     }
-
-
 }
-
